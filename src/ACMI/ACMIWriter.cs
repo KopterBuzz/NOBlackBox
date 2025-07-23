c225e43dfced3b257d79f8f8734b4eacec5973a5
Gabor Nemeth <etgabo@gmail.com>
2025-07-23 20:58:58 +0100

    increment version number

--- src/ACMI/ACMIWriter.cs
+++ src/ACMI/ACMIWriter.cs
@@ -47,7 +47,7 @@ namespace NOBlackBox
             {
                 { "ReferenceTime", reference.ToString("s") + "Z" },
                 { "DataSource", $"Nuclear Option {Application.version}" },
-                { "DataRecorder", $"NOBlackBox 0.3.5" },
+                { "DataRecorder", $"NOBlackBox 0.3.6" },
                 { "Author", GameManager.LocalPlayer.PlayerName.Replace(",", "\\,") },
                 { "RecordingTime", DateTime.Now.ToString("s") + "Z" },
 				{ "MapId", $"NuclearOption.{currentMapKey.Path}"},
